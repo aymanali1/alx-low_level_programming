86004faf67299a377109d7aad350b3275a1da623
ayman ali <aymanali2193@gmail.com>
2023-12-25 01:41:01 +0200

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,13 @@ int main(void)
 {
 	char ch = 'a';
 
-	while (ch <= 'z' && ch != 'q' || ch != 'e')
+	while (ch <= 'z')
 	{
-		putchar(ch);
-		ch++;
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+			ch++;
+		}
 	}
 
 	putchar('\n');
