8ffbac9dc3fcdf613902b75795830d4a3c59fdcb
ayman ali <aymanali2193@gmail.com>
2023-12-25 05:29:40 +0200

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <string>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
