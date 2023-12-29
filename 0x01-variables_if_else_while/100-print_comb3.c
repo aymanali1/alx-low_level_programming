f89745dcb2914a6bd91599195ad2f5979861d38d
ayman ali <aymanali2193@gmail.com>
2023-12-30 00:45:57 +0200

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,16 +5,16 @@
  *
  * Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
 	int i;
 	int j;
 
 	for (i = 48; i < 57; i++)
 	{
-		for (j = i+1; j <= 57; j++)
+		for (j = i + 1; j <= 57; j++)
 		{
-			if ( i != j)
+			if (i != j)
 			{
 				putchar(i);
 				putchar(j);
