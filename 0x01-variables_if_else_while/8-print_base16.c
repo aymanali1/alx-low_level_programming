9d0ec993f583fd78c6b07b41beaa4578bd36528a
ayman ali <aymanali2193@gmail.com>
2023-12-29 05:01:15 +0200

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 48;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 65; i <= 70; i++)
+	{
+		putchar (i);
+	}
+	putchar('\n');
+
+	return (0);
+}
